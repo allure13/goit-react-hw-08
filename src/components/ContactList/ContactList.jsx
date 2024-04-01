be1344ba2a8a5b466c1a0604bda0f07f0ba35f33
Ihor Chepliaka <140323284+allure13@users.noreply.github.com>
2024-04-01 17:39:11 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,18 @@
+import { useSelector } from 'react-redux';
+import Contact from '../Contact/Contact';
+import css from './ContactList.module.css';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
+
+export default function ContactList() {
+  const filteredContacts = useSelector(selectFilteredContacts);
+
+  return (
+    <ul className={css.list}>
+      {filteredContacts.map(({ id, name, number }) => (
+        <li key={id} className={css.item}>
+          <Contact name={name} number={number} id={id} />
+        </li>
+      ))}
+    </ul>
+  );
+}
